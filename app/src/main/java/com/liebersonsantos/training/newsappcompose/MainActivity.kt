16f084ee447f3c449e0b0cccbe941e0902985d15
lndt <lieberson.santos@gft.com>
2023-10-20 16:21:18 -0300

    part-2- init OnBoarding Screen

--- app/src/main/java/com/liebersonsantos/training/newsappcompose/MainActivity.kt
+++ app/src/main/java/com/liebersonsantos/training/newsappcompose/MainActivity.kt
@@ -3,7 +3,12 @@ package com.liebersonsantos.training.newsappcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.liebersonsantos.training.newsappcompose.presentation.onboarding.OnBoardingScreen
 import com.liebersonsantos.training.newsappcompose.ui.theme.NewsAppComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -12,7 +17,9 @@ class MainActivity : ComponentActivity() {
         installSplashScreen()
         setContent {
             NewsAppComposeTheme {
-
+                Box(modifier = Modifier.background(color = MaterialTheme.colorScheme.background)) {
+                    OnBoardingScreen()
+                }
             }
         }
     }
