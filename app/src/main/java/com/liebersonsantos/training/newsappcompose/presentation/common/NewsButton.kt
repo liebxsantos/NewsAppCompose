8b117eb0282bef7c4a01a14244c1511584f34f9d
lndt <lieberson.santos@gft.com>
2023-10-20 17:08:09 -0300

    part-2- OnBoarding Screen - button

--- app/src/main/java/com/liebersonsantos/training/newsappcompose/presentation/common/NewsButton.kt
+++ app/src/main/java/com/liebersonsantos/training/newsappcompose/presentation/common/NewsButton.kt
@@ -5,10 +5,12 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import com.liebersonsantos.training.newsappcompose.ui.theme.WhiteGray
 
 @Composable
 fun NewButton(
@@ -27,4 +29,19 @@ fun NewButton(
             style = MaterialTheme.typography.labelMedium.copy(fontWeight = FontWeight.SemiBold)
         )
     }
+}
+
+@Composable
+fun NewsTextButton(
+    text: String,
+    onClick: () -> Unit
+) {
+    TextButton(onClick = onClick) {
+        Text(
+            text = text,
+            style = MaterialTheme.typography.labelMedium.copy(fontWeight = FontWeight.SemiBold),
+            color = WhiteGray
+        )
+    }
+
 }
