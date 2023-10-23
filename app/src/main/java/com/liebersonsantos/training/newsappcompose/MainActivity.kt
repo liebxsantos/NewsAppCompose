3e75c287134f83d47f5e825e083f67a933a1085c
lndt <lieberson.santos@gft.com>
2023-10-23 12:34:05 -0300

    part-2-windowTranslucentStatus

--- app/src/main/java/com/liebersonsantos/training/newsappcompose/MainActivity.kt
+++ app/src/main/java/com/liebersonsantos/training/newsappcompose/MainActivity.kt
@@ -8,12 +8,14 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.WindowCompat
 import com.liebersonsantos.training.newsappcompose.presentation.onboarding.OnBoardingScreen
 import com.liebersonsantos.training.newsappcompose.ui.theme.NewsAppComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         installSplashScreen()
         setContent {
             NewsAppComposeTheme {
